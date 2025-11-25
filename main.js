454d3b54595ea8718233d10420203fda59178349
Dumv <du132013@gmail.com>
2025-11-25 10:03:07 +0700

    d

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+function showToast(message) {
+    const toast = document.getElementById("toast");
+    toast.textContent = message;
+    toast.style.opacity = "1";
+
+    setTimeout(() => {
+        toast.style.opacity = "0";
+    }, 1500);
+}
+
 async function loadFiles(path, containerId) {
     const owner = "tientai15468";
     const repo = "tainguyen";
-    const branch = "main"; // quan trọng
+    const branch = "main";
 
     const url = `https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${branch}`;
 
@@ -19,6 +29,13 @@ async function loadFiles(path, containerId) {
         if (file.type === "file") {
             const img = document.createElement("img");
             img.src = file.download_url;
+
+            // Khi bấm vào ảnh → copy link
+            img.onclick = () => {
+                navigator.clipboard.writeText(file.download_url);
+                showToast("Đã copy link ảnh!");
+            };
+
             container.appendChild(img);
         }
     });
