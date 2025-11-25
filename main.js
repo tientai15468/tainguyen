d58cffcda09025163cda446206cdd689c2c18606
Dumv <du132013@gmail.com>
2025-11-25 10:05:45 +0700

    d1

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function showToast(message) {
     }, 1500);
 }
 
-async function loadFiles(path, containerId) {
+async function loadFiles(path, containerId, type) {
     const owner = "tientai15468";
     const repo = "tainguyen";
     const branch = "main";
@@ -27,18 +27,36 @@ async function loadFiles(path, containerId) {
 
     files.forEach(file => {
         if (file.type === "file") {
-            const img = document.createElement("img");
-            img.src = file.download_url;
 
-            // Khi bấm vào ảnh → copy link
-            img.onclick = () => {
-                navigator.clipboard.writeText(file.download_url);
-                showToast("Đã copy link ảnh!");
-            };
+            // LOAD ẢNH
+            if (type === "image" && file.name.match(/\.(jpg|jpeg|png|webp)$/i)) {
+                const img = document.createElement("img");
+                img.src = file.download_url;
 
-            container.appendChild(img);
+                img.onclick = () => {
+                    navigator.clipboard.writeText(file.download_url);
+                    showToast("Đã copy link ảnh!");
+                };
+
+                container.appendChild(img);
+            }
+
+            // LOAD VIDEO
+            if (type === "video" && file.name.endsWith(".mp4")) {
+                const video = document.createElement("video");
+                video.src = file.download_url;
+                video.controls = true;
+
+                video.onclick = () => {
+                    navigator.clipboard.writeText(file.download_url);
+                    showToast("Đã copy link video!");
+                };
+
+                container.appendChild(video);
+            }
         }
     });
 }
 
-loadFiles("image", "image-list");
+loadFiles("image", "image-list", "image");
+loadFiles("video", "video-list", "video");
