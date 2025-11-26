f0176ffa97fd949a9ab89ffcae4b5a69753b7139
Dumv <du132013@gmail.com>
2025-11-26 15:20:23 +0700

    updates

---
+++ main_game.js
@@ -0,0 +1,44 @@
+function showToast(message) {
+    const toast = document.getElementById("toast");
+    toast.textContent = message;
+    toast.style.opacity = "1";
+
+    setTimeout(() => {
+        toast.style.opacity = "0";
+    }, 1500);
+}
+
+async function loadFolders(path, containerId) {
+    const owner = "tientai15468";
+    const repo = "tainguyen";
+    const branch = "main";
+
+    const url = `https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${branch}`;
+
+    const res = await fetch(url);
+    const items = await res.json();
+
+    if (!Array.isArray(items)) {
+        console.error("API error:", items);
+        return;
+    }
+
+    const container = document.getElementById(containerId);
+
+    items.forEach(item => {
+        if (item.type === "dir") {
+            const div = document.createElement("div");
+            div.textContent = item.name;
+
+            div.onclick = () => {
+                navigator.clipboard.writeText(item.path);
+                showToast("Đã copy tên folder!");
+            };
+
+            container.appendChild(div);
+        }
+    });
+}
+
+// GỌI HÀM
+loadFolders("gameandroi", "folder-list");
