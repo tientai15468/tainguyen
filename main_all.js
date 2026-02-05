012014f5c2d07892fa8fc6e8d583812bc3e30dbc
Dumv <du132013@gmail.com>
2026-02-05 16:23:19 +0700

    d

--- main_all.js
+++ main_all.js
@@ -50,3 +50,53 @@ async function loadFolders(path, containerId) {
     }
 }
 
+
+async function loadFiles(path, containerId, type) {
+    const owner = "tientai15468";
+    const repo = "tainguyen";
+    const branch = "main";
+
+    const url = `https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${branch}`;
+
+    const res = await fetch(url);
+    const files = await res.json();
+
+    if (!Array.isArray(files)) {
+        console.error("API error:", files);
+        return;
+    }
+
+    const container = document.getElementById(containerId);
+
+    files.forEach(file => {
+        if (file.type === "file") {
+
+            // LOAD ẢNH
+            if (type === "image" && file.name.match(/\.(jpg|jpeg|png|webp)$/i)) {
+                const img = document.createElement("img");
+                img.src = file.download_url;
+
+                img.onclick = () => {
+                    navigator.clipboard.writeText(file.download_url);
+                    showToast("Đã copy link ảnh!");
+                };
+
+                container.appendChild(img);
+            }
+
+            // LOAD VIDEO
+            if (type === "video" && file.name.endsWith(".mp4")) {
+                const video = document.createElement("video");
+                video.src = file.download_url;
+                video.controls = true;
+
+                video.onclick = () => {
+                    navigator.clipboard.writeText(file.download_url);
+                    showToast("Đã copy link video!");
+                };
+
+                container.appendChild(video);
+            }
+        }
+    });
+} 
